ab5e3eb0815f3715be858a521f7f38ac3f21041f
Zahid4960 <zahidcse4960@gmail.com>
2018-12-07 23:24:38 +0600

    Created delete edit functionality

--- app/controllers/buckets_controller.rb
+++ app/controllers/buckets_controller.rb
@@ -1,6 +1,52 @@
 class BucketsController < ApplicationController
 
+	before_action :find_bucket, only: [:show, :edit, :update, :destroy]
+
 	def index
+		@buckets = Bucket.all.order("created_at DESC")
+	end	
+
+	def show
+	end
+
+	def new
+		@bucket = Bucket.new
+	end
+
+	def create
+		@bucket = Bucket.new(bucket_params)
+
+		if @bucket.save
+			redirect_to @bucket, notice: "Successfully created new Bucket"
+
+		else
+			render 'new'
+		end
+	end
+
+	def update
+		if @bucket.update(bucket_params)
+			redirect_to @bucket, notice: "Bucket was Successfully updated!"
+
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@bucket.destroy
+		redirect_to root_path
+	end
+
+
+	private
+
+	def bucket_params
+		params.require(:bucket).permit(:title, :description)
+	end
+
+	def find_bucket
+		@bucket = Bucket.find(params[:id])
 	end
 	
 end
